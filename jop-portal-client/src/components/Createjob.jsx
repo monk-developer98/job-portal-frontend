b05b7c7b3a760c4bb9697fc7adaabf4299a2bcf3
Ashish Saxena <ashish.sxn847@gmail.com>
2025-03-05 12:39:28 +0530

    done frontend

--- jop-portal-client/src/components/Createjob.jsx
+++ jop-portal-client/src/components/Createjob.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useForm } from 'react-hook-form'
+import CreatableSelect from 'react-select/creatable'
 
 const Createjob = () => {
     const {register, handleSubmit , formState:{errors} } = useForm()
 
     const onSubmit = (data) => {}
+    const [selectedOption, setSelectedOption] = useState(null);
+
+    const handleChange = (newValue) => {
+        setSelectedOption(newValue);
+      };
+
+    const options = [
+        { value: 'Javascript', label: 'Javascript' },
+        { value: 'C++', label: 'C++' },
+        { value: 'HTML', label: 'HTML' },
+        { value: 'CSS', label: 'CSS' },
+        { value: 'ReacT', label: 'Rreact' },
+        { value: 'Node', label: 'Node' },
+        { value: 'Mongo DB', label: 'Mongo Db' },
+        { value: 'Redux', label: 'Redux' },
+        { value: 'MERN', label: 'MERN' },
+    ]
   return (
     <div className='max-w-screen-2xl container mx-auto xl:px-24 px-4'>
         <div className='bg-[#FAFAFA] py-10 px-4 lg:px-16'>
@@ -64,6 +82,43 @@ const Createjob = () => {
                             <option value='Work remotely'>Work remotely</option>
                        </select>
                     </div>
+                </div>
+
+                <div>
+                    <label className='block mb-2 text-lg'>Experience Level</label>
+                    <CreatableSelect 
+                        className='create-job-input py-4' 
+                        isMulti
+                        options={options}
+                        value={selectedOption}
+                        onChange={handleChange}
+                    />
+                </div>
+
+                <div className='create-job-flex'>
+                    <div className='lg:w-1/2 w-full'>
+                        <label className='block mb-2 text-lg'>Company Logo</label>
+                        <input type='url' placeholder='Paste your company logo url: img1.jpg'  className='create-job-input' {...register('companyLogo')} />
+                    </div>
+
+                    <div className='lg:w-1/2 w-full'>
+                        <label className='block mb-2 text-lg'>Employment Type</label>
+                       <select {...register('employmentType')} className='create-job-input'>
+                            <option value=''>Select Your Employment Type</option>
+                            <option value='Full-time'>Full Time</option>
+                            <option value='Part-time'>Part Time</option>
+                            <option value='Temporary'>Temporary</option>
+                       </select>
+                    </div>
+                </div>
+
+                <div className='w-full'>
+                    <label className='block mb-2 text-lg'>Job Descritpion</label>
+                    <textarea className='w-full pl-3 py-1.5 focus:outline-none ' rows={6} placeholder='Job Description' {...register('description')} />
+                </div>
+                <div className='w-full'>
+                    <label className='block mb-2 text-lg'>Job Posted By</label>
+                    <input className='create-job-input' type='email'  placeholder='your email' {...register('postedBy')} />
                 </div>
 
                 <input type='submit' className='block mt-12  bg-blue text-white font-semibold px-8 py-2 rounded-sm  cursor-pointer' />
